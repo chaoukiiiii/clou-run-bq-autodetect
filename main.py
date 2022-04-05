6b480cf4ba5266a0a0f1ca943d4fc7b3c9236a1d
chaoukiiiii <chaoukitrabelsi279@gmail.com>
2022-04-05 13:34:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,7 @@ def entry():
         return ("Error  there is No Dataset matchs the provided dataset variable.", 500)
     
     l=pd.read_csv(uri, sep=delimiter, index_col=0, nrows=0).columns.tolist()
+    print(l)
     s=[bigquery.SchemaField(i, "STRING") for i in l]
     # Setup the job to append to the table if it already exists and to autodetect the schema
     job_config = bigquery.LoadJobConfig(
