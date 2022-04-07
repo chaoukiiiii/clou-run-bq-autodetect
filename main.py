7a52810e16f30bc8794de76da3772e2b6711e888
chaoukiiiii <chaoukitrabelsi279@gmail.com>
2022-04-07 12:58:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def entry():
     source_format=bigquery.SourceFormat.CSV,
     field_delimiter=delimiter,
     skip_leading_rows=1,
-    #allow_jagged_rows=True,
+    allow_jagged_rows=True,
     allow_quoted_newlines=True
     )
 
