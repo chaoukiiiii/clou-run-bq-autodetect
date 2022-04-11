8073047f44df828d070265bfb3d8e2c2547b4115
chaoukiiiii <chaoukitrabelsi279@gmail.com>
2022-04-11 10:11:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -79,8 +79,8 @@ def entry():
     field_delimiter=delimiter,
     skip_leading_rows=1,
     allow_jagged_rows=True,
-    #allow_quoted_newlines=True,
-    quote_character="¤"
+    allow_quoted_newlines=True
+    #quote_character="¤"
     )
 
     # Run the load job
